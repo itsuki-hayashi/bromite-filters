f83e58f752ef9cb36aa2a78369acbb264120ff29
zhiyuan-lin <2121340+zhiyuan-lin@users.noreply.github.com>
2021-08-07 00:57:28 +0900

    Add ruleset_converter binary

--- update-filters.sh
+++ update-filters.sh
@@ -19,7 +19,7 @@ wget -O filters/vietnamese.txt https://raw.githubusercontent.com/abpvn/abpvn/mas
 filters=($(ls -d filters/*))
 input_files=$(IFS=, ; echo "${filters[*]}")
 
-ruleset_converter --input_format=filter-list \
+./ruleset_converter --input_format=filter-list \
 	--output_format=unindexed-ruleset \
     --input_files=$input_files \
 	--output_file=filters.dat
