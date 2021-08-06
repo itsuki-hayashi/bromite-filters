70571e2808f75f5ad0d84ec2a9d7b43db5ba3ae6
zhiyuan-lin <2121340+zhiyuan-lin@users.noreply.github.com>
2021-08-07 00:31:13 +0900

    Simplify compile-filters.sh

--- compile-filters.sh
+++ compile-filters.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+
+filters=($(ls -d filters/*))
+input_files=$(IFS=, ; echo "${filters[*]}")
+
 ruleset_converter --input_format=filter-list \
 	--output_format=unindexed-ruleset \
-    --input_files=filters/annoyances.txt,filters/chinese.txt,filters/dutch.txt,filters/english.txt,filters/french.txt,filters/german.txt,\
-filters/japanese.txt,filters/korean.txt,filters/mobile.txt,filters/russian.txt,filters/social.txt,filters/spanish.txt,filters/spyware.txt,\
-filters/track-param.txt,filters/turkish.txt,filters/vietnamese.txt \
+    --input_files=$input_files \
 	--output_file=filters.dat
